aa4a5893c9ca2c2c614b9e23c4b4f4ca4d21075e
SHAYAN_KHAN <116594938+shayan-noor3060@users.noreply.github.com>
2024-05-17 14:42:48 +0500

    instagram ui

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _MyAppState extends State<MyApp> {
             ],
           ),
           bottomNavigationBar: const BottomAppBar(
+            height: 40,
             child:  Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children:  [
